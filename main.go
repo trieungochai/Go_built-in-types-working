4dbff6483a79c0ec526180862229360aba46af0a
trieungochai <trieungochai.dev@gmail.com>
2021-10-27 17:58:39 +0700

    Reference Types - Maps

--- main.go
+++ main.go
@@ -2,49 +2,32 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 // reference types (pointers, slices, maps, functions, channels)
 // interface types
 
 func main() {
-	var animals []string
+	intMap := make(map[string]int)
 
-	animals = append(animals, "dog")
-	animals = append(animals, "cat")
-	animals = append(animals, "frog")
-	animals = append(animals, "fish")
-	animals = append(animals, "pig")
+	intMap["one"] = 1
+	intMap["two"] = 2
+	intMap["three"] = 3
+	intMap["four"] = 4
+	intMap["five"] = 5
 
-	fmt.Println(animals)
-
-	for _, x := range animals {
-		fmt.Println(x)
+	for key, value := range intMap {
+		fmt.Println(key, value)
 	}
 
-	fmt.Println("Element 0 is", animals[0])
-	fmt.Println("First two element are", animals[0:2])
-	fmt.Println("The slice is", len(animals), "elements long")
-	fmt.Println("Is it sorted?", sort.StringsAreSorted(animals))
-
-	sort.Strings(animals)
-	fmt.Println("Is it sorted now?", sort.StringsAreSorted(animals))
-	fmt.Println(animals)
-
-	DeleteFromSlice(animals, 1)
-	fmt.Println(animals)
-}
-
-func DeleteFromSlice(a []string, i int) []string {
-	// copy the last element to index i
-	a[i] = a[len(a)-1]
-
-	// erase the last element by giving it its default empty value
-	a[len(a)-1] = ""
-
-	// truncate the slice by deleting the last element
-	a = a[:len(a)-1]
-
-	return a
+	delete(intMap, "four")
+	// for key, value := range intMap {
+	// 	fmt.Println(key, value)
+	// }
+	el, ok := intMap["four"]
+	if ok {
+		fmt.Println(el, "is in map.")
+	} else {
+		fmt.Println(el, "is not in map.")
+	}
 }
