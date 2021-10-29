24be72f672a95af17db2711c435f5ec39ee434bb
trieungochai <trieungochai.dev@gmail.com>
2021-10-29 15:12:02 +0700

    Reference Types - Channels

--- main.go
+++ main.go
@@ -5,35 +5,15 @@ import "fmt"
 // reference types (pointers, slices, maps, functions, channels)
 // interface types
 
-type Animals struct {
-	Name         string
-	Sound        string
-	NumberOfLegs int
-}
-
-func (a *Animals) Says() {
-	fmt.Printf("A %s says %s", a.Name, a.Sound)
-	fmt.Println()
-}
-
-func (a *Animals) HowmanyLegs() {
-	fmt.Printf("A %s has %d legs", a.Name, a.NumberOfLegs)
-	fmt.Println()
-}
-
 func main() {
-	var dog Animals
-	dog.Name = "dog"
-	dog.Sound = "woof"
-	dog.NumberOfLegs = 4
-	dog.Says()
-	dog.HowmanyLegs()
+	go doSomething("Hello World!!!")
 
-	cat := Animals{
-		Name:         "cat",
-		Sound:        "moew",
-		NumberOfLegs: 4,
+	for {
 	}
-	cat.Says()
-	cat.HowmanyLegs()
+}
+
+func doSomething(s string) {
+	for {
+		fmt.Println("s is", s)
+	}
 }
